759d796d42262d4ccdd8225e78c3c1a08065a8ae
huynhducthanhtuan <huynhducthanhtuan@gmail.com>
2025-08-08 16:40:38 +0700

    Configure CORS in .NET app to allow all origins

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,6 +2,16 @@ using TuanHuynh;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Add services to the container.
 builder.Services.AddSingleton<HelloWorld>();
 
@@ -10,6 +20,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
